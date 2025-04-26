da060e56a10b35f2872c6a4a107b8be07dc27f16
Nikola Dojcinovic <goonergalactico@gmail.com>
2025-04-26 23:24:34 +0200

    add extensions dinamically, delete extension & switch active/inactive extensions

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const toggleIconElement = document.querySelector('#toggle-icon');
 const toggleIconImageElement = document.querySelector('#toggle-icon-image');
 const extensionsLogoElement = document.querySelector('#extensions-logo');
 
+const extensionsWrapperElement = document.querySelector('#extensions-wrapper');
+const extensionsCountElement = document.querySelector('#extensions-count');
+
 toggleIconElement.addEventListener('click', handleThemeToggle);
 
 function handleThemeToggle() {
@@ -17,3 +20,63 @@ function handleThemeToggle() {
         ? "./assets/images/logo.svg"
         : "./assets/images/logo-dark.svg";
 }
+
+let extensionsData = [];
+
+async function fetchData() {
+    try {
+        const response = await fetch('./data.json');
+        const data = await response.json();
+        extensionsData = data;
+
+        renderExtensions();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function renderExtensions() {
+    extensionsWrapperElement.innerHTML = ''
+    extensionsData.forEach((item, idx) => {
+        const extension = document.createElement('div');
+        extension.className = 'extension';
+
+        extension.innerHTML = `
+            <div class="extension-content">
+                <img
+                class="extension-img"
+                src="${item.logo}"
+                alt="extension-icon"
+                />
+                <div class="extension-text">
+                <h4>${item.name}</h4>
+                <p>${item.description}</p>
+                </div>
+            </div>
+            <div class="extension-actions">
+                <button class="btn" onClick="handleDelete(${idx})">Remove</button>
+                <label class="switch">
+                <input type="checkbox" onchange="handleCheckboxChange(event, ${idx})" ${item.isActive ? "checked" : null}/>
+                <span class="slider round"></span>
+                </label>
+            </div>
+        `;
+
+        extensionsWrapperElement.appendChild(extension);
+        extensionsCountElement.textContent = `Total extensions (${extensionsData.length})`
+    });
+}
+
+fetchData();
+
+function handleCheckboxChange(e, id) {
+    extensionsData[id].isActive = e.target.checked
+}
+
+function handleDelete(id) {
+    extensionsData.splice(id, 1)
+    renderExtensions()
+}
+
+
+
