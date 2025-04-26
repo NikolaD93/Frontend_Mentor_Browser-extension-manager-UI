3d305bfe729da6afcdd9784234445dd0b57a8501
Nikola Dojcinovic <goonergalactico@gmail.com>
2025-04-26 20:18:42 +0200

    navbar finish

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-// fetch('./data.json').then(res => res.json()).then(data => console.log(data))
-import { data } from './data.js';
-console.log(data)
+const toggleIconElement = document.querySelector('#toggle-icon');
+const toggleIconImageElement = document.querySelector('#toggle-icon-image');
+const extensionsLogoElement = document.querySelector('#extensions-logo');
 
-localStorage.setItem('name', 'Obaseki Nosa');
+toggleIconElement.addEventListener('click', handleThemeToggle);
+
+function handleThemeToggle() {
+    document.body.classList.toggle('dark');
+
+    console.log(toggleIconImageElement.src.includes("icon-moon.svg"))
+
+    toggleIconImageElement.src = toggleIconImageElement.src.includes("icon-moon.svg")
+        ? "./assets/images/icon-sun.svg"
+        : "./assets/images/icon-moon.svg";
+
+    extensionsLogoElement.src = extensionsLogoElement.src.includes("logo-dark.svg")
+        ? "./assets/images/logo.svg"
+        : "./assets/images/logo-dark.svg";
+}
