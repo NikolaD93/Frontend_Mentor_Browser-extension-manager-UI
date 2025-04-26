efb199b49fec9965de31d481055629d46e30830e
Nikola Dojcinovic <goonergalactico@gmail.com>
2025-04-26 19:10:59 +0200

    project setup

---
+++ app.js
@@ -0,0 +1,5 @@
+// fetch('./data.json').then(res => res.json()).then(data => console.log(data))
+import { data } from './data.js';
+console.log(data)
+
+localStorage.setItem('name', 'Obaseki Nosa');
